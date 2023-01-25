0b0eafa4dc4a30d6682a4d164bea05852c3d7d00
Natalya <ne.emelkina@gmail.com>
2023-01-25 20:17:30 +0300

    Task_41

---
+++ Task_41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int [] Array = CreateArray();
+Console.WriteLine ("Введённые числа:");
+ShowArray(Array);
+Console.WriteLine ("Количество введенных чисел больше 0:");
+CountPositive (Array);
+Console.WriteLine(CountPositive (Array));
+
+int[] CreateArray()
+{
+    Console.WriteLine("Введите количество элементов для анализа");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {   
+        Console.WriteLine($"Введите {i+1} элемент");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+
+
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int CountPositive (int[] array)
+{
+    int count = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0) 
+        count++;
+    }
+    return count;
+}
